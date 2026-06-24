0acb9210126c20ea1a06cb600eca4aa7348d1262
Naman Awana <namanawana000@gmail.com>
2026-06-24 13:03:06 +0530

    feat: WebSocket server + ConnectionManager

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException,WebSocket,WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from models import FeatureFlag, ConfigVar, ConfigUpdate
 from FlagStore import FlagStore
@@ -20,6 +20,27 @@ flag_store.load_flags()
 config_store=ConfigStore()
 config_store.load_configs()
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections:list[WebSocket]=[]
+    async def connect(self, websocket:WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+    def disconnect(self, websocket:WebSocket):
+        self.active_connections.remove(websocket)
+    async def broadcast(self, message:dict):
+        for connection in self.active_connections:
+            await connection.send_json(message)
+
+manager=ConnectionManager()   
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)  
+    try:
+        while True:
+            await websocket.receive_text()           
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
 @app.get("/flags")
 def get_flags():
     return list(flag_store.flags.values())
@@ -33,10 +54,14 @@ def create_flag(flag:FeatureFlag):
     return flag 
 
 @app.patch("/flags/{name}/toggle")
-def toggle_flag(name:str):
+async def toggle_flag(name:str):
     if name not in flag_store.flags:
         raise HTTPException(status_code=404, detail="Flag not found")
     updated_flag = flag_store.toggle_flag(name)
+    await manager.broadcast({
+        "type":"flag updated",
+        "flag":updated_flag.model_dump()
+    })
     return updated_flag
 
 @app.delete("/flags/{name}")
